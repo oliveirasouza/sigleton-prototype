e67b6725107c56f985cde4f1806ef57fe54d1fe5
francisco.sousa <francisco.sousa@cgi.com>
2025-02-14 14:12:39 +0000

    Primeiro Commit

---
+++ src/main/java/spring/sigleton_prototype/SigletonPrototypeApplication.java
@@ -0,0 +1,13 @@
+package spring.sigleton_prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigletonPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigletonPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/sigleton_prototype/SigletonPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.sigleton_prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigletonPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
