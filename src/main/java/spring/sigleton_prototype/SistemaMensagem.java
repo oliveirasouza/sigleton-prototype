a0cb13ed75e6affc107365e176bef7ae51ad8742
francisco.sousa <francisco.sousa@cgi.com>
2025-02-14 14:57:18 +0000

    Atualizacao Sistema Mensagem

--- src/main/java/spring/sigleton_prototype/SistemaMensagem.java
+++ src/main/java/spring/sigleton_prototype/SistemaMensagem.java
@@ -1,4 +1,18 @@
 package spring.sigleton_prototype;
 
 public class SistemaMensagem {
+    private Remetente noreply;
+    private Remetente financeiro;
+
+    public void enviarConfirmacaoCadastro() {
+        System.out.println(noreply);
+        System.out.println("Seu cadastro foi aprovado!");
+    }
+
+
+    public void enviarMensagemBoasVindas() {
+        financeiro.setEmail("tech@dio.com.br");
+        System.out.println(financeiro);
+        System.out.println("Bem-vindo à Tech Elite");
+    }
 }
